bf41c71e49ed02cba13dca358fd4edb37b9b152c
rajdev9101 <devraj93957@gmail.com>
2025-07-21 12:32:17 +0530

    Update info.py

--- info.py
+++ info.py
@@ -59,7 +59,7 @@ SUPPORT_GROUP = int(environ.get('SUPPORT_GROUP', '-1002255543570'))
 request_channel = environ.get('REQUEST_CHANNEL', '-1002568244043')
 REQUEST_CHANNEL = int(request_channel) if request_channel and id_pattern.search(request_channel) else None
 UPI_PAY_LOGS = int(environ.get('UPI_PAY_LOGS', '-1002870973266'))
-MOVIE_UPDATE_CHANNEL = int(environ.get('MOVIE_UPDATE_CHANNEL', '-1002441285313'))
+MOVIE_UPDATE_CHANNEL = int(environ.get('MOVIE_UPDATE_CHANNEL', '-1002465960701'))
 
 AUTO_FILTER = is_enabled('AUTO_FILTER', True)
 PORT = os.environ.get('PORT', '5000')
